54d914970e305b307c4709b057c845c008cb3631
eryczh <ericandrade2810@gmail.com>
2023-08-22 10:55:52 -0300

    fechamento do dia

--- projeto_revisao_ed/TesteArrayList.java
+++ projeto_revisao_ed/TesteArrayList.java
@@ -8,7 +8,30 @@ public class TesteArrayList {
         ArrayList<String> arrayList = new ArrayList<String>();
 
         arrayList.add("Elemento A");
-        arrayList.add("Elemento B");
+        arrayList.add("Elemento C");
+
+        System.out.println(arrayList);
+
+        arrayList.add(1, "Elemento B ");
+
+        System.out.println(arrayList);
+
+        boolean existe = arrayList.contains("Elemento C");
+        if (existe) {
+            System.out.println("Elemento encontrado no vetor");
+        } else {
+            System.out.println("Elemento não existe no vetor");
+        }
+
+        int posicao = arrayList.indexOf("Elemento C");
+        if (posicao > -1) {
+            System.out.println("Elemento encontrado no index n° " + posicao);
+        } else {
+            System.out.println("Elemento não existe no vetor");
+        }
+
+        arrayList.remove(0);
+        arrayList.remove("Elemento C");
 
         System.out.println(arrayList);
     }
